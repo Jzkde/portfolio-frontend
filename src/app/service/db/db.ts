6c6e1cfd41f1a5a73ad48ed40d6f2752af515276
jzkde <dagjjj@gmail.com>
2024-08-26 00:18:06 -0300

    Actualizando Angular

--- src/app/service/db/db.ts
+++ src/app/service/db/db.ts
@@ -1,7 +1,7 @@
 export const Db = {
 
-  baseApi: "http://localhost:8081",
- // baseApi:'https://port-back-c0ij.onrender.com',
+ // baseApi: "http://localhost:8081",
+  baseApi:'https://port-back-c0ij.onrender.com',
 
   finalApi() {
     return `${this.baseApi}/api/`;
